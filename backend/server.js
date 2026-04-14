dfae6090f742da3b8cf51f0de41010419040d5ec
rudraparekh04 <rudraparekh04@gmail.com>
2026-04-14 22:13:14 +0530

    fix in server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,16 @@ const userRoutes = require('./routes/user.routes');
 const { errorHandler } = require('./middleware/error.middleware');
 
 const app = express();
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "https://production-auth-in-mern.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    return res.sendStatus(204); // ✅ STOP HERE
+  }
+  next();
+});
 app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true,
@@ -43,10 +53,10 @@ app.use((err, req, res, next) => {
 //   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
 //   return res.sendStatus(200);
 // });
-app.options("*", cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
+// app.options("*", cors({
+//   origin: process.env.CLIENT_URL,
+//   credentials: true
+// }));
 
 // Body parsing
 app.use(express.json({ limit: '10mb' }));
